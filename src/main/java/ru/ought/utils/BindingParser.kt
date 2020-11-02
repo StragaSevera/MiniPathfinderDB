5bb53dd209f2692a09b596a100b9ac1530d10548
StragaSevera <ought@yandex.ru>
2020-11-03 00:20:43 +0300

    Refactor parse method

--- src/main/java/ru/ought/utils/BindingParser.kt
+++ src/main/java/ru/ought/utils/BindingParser.kt
@@ -3,15 +3,17 @@ package ru.ought.utils
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import org.dom4j.Attribute
+import org.dom4j.Document
 import org.dom4j.DocumentHelper
 import org.dom4j.Element
 import java.io.InputStream
 
 class BindingParser(data: InputStream) {
-    constructor(resourceName: String): this(BindingParser::class.java.getResourceAsStream(resourceName))
+    constructor(resourceName: String) : this(BindingParser::class.java.getResourceAsStream(resourceName))
+
     private val dataString: String = data.reader().readText()
 
-    fun <T: Parent> load(): T {
+    fun <T : Parent> load(): T {
         val root = FXMLLoader().load<T>(parse().byteInputStream())
         BindingManager.performBinding(root)
         return root
@@ -21,9 +23,19 @@ class BindingParser(data: InputStream) {
         val fxmlData = extractMetadata(dataString)
         val document = DocumentHelper.parseText(fxmlData.xml)
 
+        addController(document)
+        addBindingProperties(document)
+        addPlaceholderSymbols(document)
+
+        return insertMetadata(document.asXML(), fxmlData.metadata)
+    }
+
+    private fun addController(document: Document) {
         val rootNode = document.rootElement
         rootNode.addAttribute("BindingManager.controller", "\$controller")
+    }
 
+    private fun addBindingProperties(document: Document) {
         val bindingNodes = document.selectNodes("//*[@*='~']")
         for (node in bindingNodes) {
             if (node is Element) {
@@ -36,7 +48,9 @@ class BindingParser(data: InputStream) {
                 propertiesBinding.forEach { node.remove(it) }
             }
         }
+    }
 
+    private fun addPlaceholderSymbols(document: Document) {
         val replaceNodes = document.selectNodes("//*[@*[starts-with(.,'~~')]]")
         for (node in replaceNodes) {
             if (node is Element) {
@@ -45,11 +59,10 @@ class BindingParser(data: InputStream) {
                 propertiesBinding.forEach { it.value = it.value.dropLast(1) }
             }
         }
-
-        return insertMetadata(document.asXML(), fxmlData.metadata)
     }
 
     private data class FxmlData(val xml: String, val metadata: String)
+
     private val regexpMetadata = """<\?import.*\?>""".toRegex()
     private fun extractMetadata(dataString: String): FxmlData {
         val metadatas = mutableListOf<String>()
